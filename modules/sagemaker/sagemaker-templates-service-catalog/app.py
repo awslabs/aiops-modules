18dcb26b3be70ca2ca69980efa407ccbf04c4201
rtdurga <102053670+rtdurga@users.noreply.github.com>
2024-03-21 12:05:17 +0530

    Update app.py
    Added vpc and subnetid parameters.

--- modules/sagemaker/sagemaker-templates-service-catalog/app.py
+++ modules/sagemaker/sagemaker-templates-service-catalog/app.py
@@ -4,7 +4,6 @@
 import os
 
 import aws_cdk
-import cdk_nag
 
 from stack import ServiceCatalogStack
 
@@ -29,9 +28,16 @@ environment = aws_cdk.Environment(
 portfolio_name = os.getenv(_param("PORTFOLIO_NAME"), DEFAULT_PORTFOLIO_NAME)
 portfolio_owner = os.getenv(_param("PORTFOLIO_OWNER"), DEFAULT_PORTFOLIO_OWNER)
 portfolio_access_role_arn = os.getenv(_param("PORTFOLIO_ACCESS_ROLE_ARN"))
+pre_prod_vpc_id = os.getenv(_param("PRE_PROD_VPC_ID"))
+pre_prod_private_subnet_ids = os.getenv(_param("PRE_PROD_PRIVATE_SUBNET_IDS"), "").split(",")
+pre_prod_public_subnet_ids = os.getenv(_param("PRE_PROD_PUBLIC_SUBNET_IDS"), "").split(",")
+prod_vpc_id = os.getenv(_param("PROD_VPC_ID"))
+prod_private_subnet_ids = os.getenv(_param("PROD_PRIVATE_SUBNET_IDS"), "").split(",")
+prod_public_subnet_ids = os.getenv(_param("PROD_PUBLIC_SUBNET_IDS"), "").split(",")
 
 if not portfolio_access_role_arn:
     raise ValueError("Missing input parameter portfolio-access-role-arn")
+
 
 app = aws_cdk.App()
 stack = ServiceCatalogStack(
@@ -40,6 +46,12 @@ stack = ServiceCatalogStack(
     portfolio_name=portfolio_name,
     portfolio_owner=portfolio_owner,
     portfolio_access_role_arn=portfolio_access_role_arn,
+    pre_prod_vpc_id=pre_prod_vpc_id,
+    pre_prod_private_subnet_ids=pre_prod_private_subnet_ids,
+    pre_prod_public_subnet_ids=pre_prod_public_subnet_ids,
+    prod_vpc_id=prod_vpc_id,
+    prod_private_subnet_ids=prod_private_subnet_ids,
+    prod_public_subnet_ids=prod_public_subnet_ids,
 )
 
 
@@ -53,7 +65,5 @@ aws_cdk.CfnOutput(
         }
     ),
 )
-
-aws_cdk.Aspects.of(stack).add(cdk_nag.AwsSolutionsChecks(log_ignores=True))
 
 app.synth()
