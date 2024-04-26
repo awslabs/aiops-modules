f39af39f7df44ebaf40093b02085806c3604bee5
saikatak <saikatak@amazon.com>
2024-04-26 13:34:47 +0530

    seedformer module for qaRagUsingAppSync

---
+++ modules/examples/llm-rag/app.py
@@ -0,0 +1,36 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+
+import aws_cdk
+from aws_cdk import App, CfnOutput
+from stack import RAGResources
+
+
+def _param(name: str) -> str:
+    return f"SEEDFARMER_PARAMETER_{name}"
+
+
+project_name = os.getenv("SEEDFARMER_PROJECT_NAME", "")
+deployment_name = os.getenv("SEEDFARMER_DEPLOYMENT_NAME", "")
+module_name = os.getenv("SEEDFARMER_MODULE_NAME", "")
+app_prefix = f"{project_name}-{deployment_name}-{module_name}"
+
+cognito_pool_id = os.getenv(_param("COGNITO_POOL_ID"))
+os_domain_endpoint = os.getenv(_param("OS_DOMAIN_ENDPOINT"))
+
+app = App()
+
+stack = RAGResources(
+    scope=app,
+    id=app_prefix,
+    cognito_pool_id=cognito_pool_id,
+    os_domain_endpoint=os_domain_endpoint,
+    env=aws_cdk.Environment(
+        account=os.environ["CDK_DEFAULT_ACCOUNT"],
+        region=os.environ["CDK_DEFAULT_REGION"],
+    ),
+)
+
+app.synth(force=True)
---
+++ modules/examples/llm-rag/stack.py
@@ -0,0 +1,48 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+from constructs import Construct
+from cdklabs.generative_ai_cdk_constructs import QaAppsyncOpensearch
+from aws_cdk import Stack
+from aws_cdk import (
+    aws_opensearchservice as os,
+    aws_cognito as cognito,
+)
+
+
+class RAGResources(Stack):
+    def __init__(self,
+                 scope: Construct,
+                 id: str,
+                 cognito_pool_id: str ,
+                 os_domain_endpoint: str,
+                 **kwargs):
+        super().__init__ \
+            (scope,
+             id,
+             description=" This stack creates resources for the LLM - QA RAG ",
+             **kwargs)
+
+        # get an existing OpenSearch provisioned cluster
+        os_domain = os.Domain.from_domain_endpoint(
+            self,
+            'osdomain',
+            # domain_arn=open_search_domain_arn, #'arn:aws:es:us-east-1:098191324367:domain/osforqadomain',
+            domain_endpoint= os_domain_endpoint ,#'https://vpc-osforqadomain-du6zvbtupkxxsk3oouvjc5tkqy.us-east-1.es.amazonaws.com',
+        )
+
+        # get an existing userpool
+        cognito_pool_id = cognito_pool_id
+        user_pool_loaded = cognito.UserPool.from_user_pool_id(
+            self,
+            'myuserpool',
+            user_pool_id=cognito_pool_id,
+        )
+
+        rag_source = QaAppsyncOpensearch(
+            self,
+            'QaAppsyncOpensearch',
+            existing_opensearch_domain=os_domain,
+            open_search_index_name='qa-appsync-index',
+            cognito_user_pool=user_pool_loaded,
+        )
