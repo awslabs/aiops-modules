f45e645a09badb1318ec2095cee4cbac362f5d3d
Anton Kukushkin <kukushkin.anton@gmail.com>
2024-05-14 13:45:19 +0100

    and ray-ml benchmark image
    Signed-off-by: Anton Kukushkin <kukushkin.anton@gmail.com>

---
+++ modules/eks/ray-ml-benchmark-image/src/pytorch_submit.py
@@ -0,0 +1,25 @@
+from ray.job_submission import JobSubmissionClient
+
+address = "http://127.0.0.1:8266"
+
+client = JobSubmissionClient(address)
+
+kick_off_pytorch_benchmark = (
+    "ray --version\n"
+    "rm -rf ray\n"
+    # Clone ray. If ray is already present, don't clone again.
+    "git clone https://github.com/ray-project/ray\n"
+    "cd ray\n"
+    "git fetch --all\n"
+    "git checkout ray-2.3.0\n"
+    # Run the benchmark.
+    " python release/air_tests/air_benchmarks/workloads/tune_torch_benchmark.py\n"
+)
+
+
+submission_id = client.submit_job(
+    entrypoint=kick_off_pytorch_benchmark,
+)
+
+print("Use the following command to follow this Job's logs:")
+print(f"ray job logs '{submission_id}' --follow --address {address}")
