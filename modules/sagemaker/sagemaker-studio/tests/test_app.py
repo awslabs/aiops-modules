911e33e83375415f7596d895f94f10bcc6f21412
Leon Luttenberger <LeonLuttenberger@users.noreply.github.com>
2024-05-20 13:35:17 -0500

    chore: Refactor sagemaker-studio inputs to use pydantic (#89)
    * chore: Refactor sagemaker-studio inputs to use pydantic
    
    * fix subnet IDs
    
    * fix subnet IDs
    
    * fix domain_name
    
    * update changelog

--- modules/sagemaker/sagemaker-studio/tests/test_app.py
+++ modules/sagemaker/sagemaker-studio/tests/test_app.py
@@ -5,6 +5,7 @@ import os
 import sys
 
 import pytest
+from pydantic import ValidationError
 
 
 @pytest.fixture(scope="function")
@@ -14,9 +15,8 @@ def stack_defaults():
     os.environ["SEEDFARMER_MODULE_NAME"] = "test-module"
     os.environ["CDK_DEFAULT_ACCOUNT"] = "111111111111"
     os.environ["CDK_DEFAULT_REGION"] = "us-east-1"
-    os.environ["SEEDFARMER_PARAMETER_RETENTION_TYPE"] = "DESTROY"
     os.environ["SEEDFARMER_PARAMETER_VPC_ID"] = "vpc-12345"
-    os.environ["SEEDFARMER_PARAMETER_PRIVATE_SUBNET_IDS"] = '["subnet-12345", "subnet-54321"]'
+    os.environ["SEEDFARMER_PARAMETER_SUBNET_IDS"] = '["subnet-12345", "subnet-54321"]'
     # Unload the app import so that subsequent tests don't reuse
     if "app" in sys.modules:
         del sys.modules["app"]
@@ -26,35 +26,22 @@ def test_app(stack_defaults):
     import app  # noqa: F401
 
 
-def test_retention_default(stack_defaults):
-    del os.environ["SEEDFARMER_PARAMETER_RETENTION_TYPE"]
-
-    with pytest.raises(Exception):
-        import app  # noqa: F401
-
-        assert os.environ["SEEDFARMER_PARAMETER_RETENTION_TYPE"] == "DESTROY"
-
-
 def test_vpc_id(stack_defaults):
     del os.environ["SEEDFARMER_PARAMETER_VPC_ID"]
 
-    with pytest.raises(Exception):
+    with pytest.raises(ValidationError):
         import app  # noqa: F401
 
-        assert os.environ["SEEDFARMER_PARAMETER_VPC_ID"] == "vpc-12345"
 
+def test_subnet_ids(stack_defaults):
+    del os.environ["SEEDFARMER_PARAMETER_SUBNET_IDS"]
 
-def test_private_subnet_ids(stack_defaults):
-    del os.environ["SEEDFARMER_PARAMETER_PRIVATE_SUBNET_IDS"]
-
-    with pytest.raises(Exception):
+    with pytest.raises(ValidationError):
         import app  # noqa: F401
-
-        assert os.environ["SEEDFARMER_PARAMETER_PRIVATE_SUBNET_IDS"] == ["subnet-12345", "subnet-54321"]
 
 
 def test_auth_mode(stack_defaults):
     os.environ["SEEDFARMER_PARAMETER_AUTH_MODE"] = "DUMMY"
 
-    with pytest.raises(Exception, match="Auth mode must be either `IAM` or `SSO`"):
+    with pytest.raises(ValidationError):
         import app  # noqa: F401
