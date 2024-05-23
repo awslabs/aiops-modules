cb9844fd41a79b7d04bb6a62013ec8cb691b9c10
saikatak <101553100+saikatak@users.noreply.github.com>
2024-05-23 14:39:50 +0530

    Added new seedfarmer module for Question and Answering Gen AI construct (#81)
    * seedformer module for qaRagUsingAppSync
    
    * Added deployspec and integrated llm-rag modules in deployment yaml
    
    * Added IAC for Question and Answering module
    
    * removed unwanted comments from the code
    
    * added READ ME and other minor corrections to paths
    
    * Added change description to CHANGELOG.md
    
    * Updated repository README.md with qna-rag module details
    
    * Corrected type in repository readme
    
    * updated urlib version
    
    * Added tests and upgdated read me
    
    * removed print statement and added open search security group as input parameter
    
    * updated example to include open search security group id
    
    * Fixed unit test failure
    
    * Moved qna-rag manifest to examples
    
    * Updated Q&A Rag ingestion module to include ingestion modules as well
    
    * deleted storage yaml from examples
    
    * updated readme and added mypy validation fixes
    
    * Updated read me to include ingestion construct and add existing S3 bucket as optional input to RAG module
    
    * Added Optional type for input asset bucket
    
    * removed the unnecessary comment
    
    * Addressed review comments - Updated IDF open search module version, Added Port as configurable parameter
    
    ---------
    
    Co-authored-by: kukushking <kukushkin.anton@gmail.com>

---
+++ modules/fmops/qna-rag/app.py
@@ -0,0 +1,74 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+
+import aws_cdk
+from aws_cdk import App
+from stack import RAGResources
+
+
+def _param(name: str) -> str:
+    return f"SEEDFARMER_PARAMETER_{name}"
+
+
+project_name = os.getenv("SEEDFARMER_PROJECT_NAME", "")
+deployment_name = os.getenv("SEEDFARMER_DEPLOYMENT_NAME", "")
+module_name = os.getenv("SEEDFARMER_MODULE_NAME", "")
+app_prefix = f"{project_name}-{deployment_name}-{module_name}"
+vpc_id = os.getenv(_param("VPC_ID"))
+cognito_pool_id = os.getenv(_param("COGNITO_POOL_ID"))
+os_domain_endpoint = os.getenv(_param("OS_DOMAIN_ENDPOINT"))
+os_domain_port = os.getenv(_param("OS_DOMAIN_PORT"), "443")
+os_security_group_id = os.getenv(_param("OS_SECURITY_GROUP_ID"))
+input_asset_bucket_name = os.getenv(_param("INPUT_ASSET_BUCKET"))
+
+if not vpc_id:
+    raise ValueError("Missing input parameter vpc-id")
+
+if not cognito_pool_id:
+    raise ValueError("Missing input parameter cognito-pool-id")
+
+if not os_domain_endpoint:
+    raise ValueError("Missing input parameter os-domain-endpoint")
+
+if not os_security_group_id:
+    raise ValueError("Missing input parameter os-security-group-id")
+
+app = App()
+
+stack = RAGResources(
+    scope=app,
+    id=app_prefix,
+    vpc_id=vpc_id,
+    cognito_pool_id=cognito_pool_id,
+    os_domain_endpoint=os_domain_endpoint,
+    os_domain_port=os_domain_port,
+    os_security_group_id=os_security_group_id,
+    os_index_name="rag-index",
+    input_asset_bucket_name=input_asset_bucket_name,
+    env=aws_cdk.Environment(
+        account=os.environ["CDK_DEFAULT_ACCOUNT"],
+        region=os.environ["CDK_DEFAULT_REGION"],
+    ),
+)
+
+assert stack.rag_ingest_resource.s3_input_assets_bucket is not None
+assert stack.rag_ingest_resource.s3_processed_assets_bucket is not None
+
+aws_cdk.CfnOutput(
+    scope=stack,
+    id="metadata",
+    value=stack.to_json_string(
+        {
+            "IngestionGraphqlApiId": stack.rag_ingest_resource.graphql_api.api_id,
+            "IngestionGraphqlArn": stack.rag_ingest_resource.graphql_api.arn,
+            "QnAGraphqlApiId": stack.rag_resource.graphql_api.api_id,
+            "QnAGraphqlArn": stack.rag_resource.graphql_api.arn,
+            "InputAssetBucket": stack.rag_ingest_resource.s3_input_assets_bucket.bucket_name,
+            "ProcessedInputBucket": stack.rag_ingest_resource.s3_processed_assets_bucket.bucket_name,
+        }
+    ),
+)
+
+app.synth(force=True)
---
+++ modules/fmops/qna-rag/stack.py
@@ -0,0 +1,103 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+from constructs import Construct
+from cdklabs.generative_ai_cdk_constructs import RagAppsyncStepfnOpensearch
+from cdklabs.generative_ai_cdk_constructs import QaAppsyncOpensearch
+from aws_cdk import Stack
+from aws_cdk import aws_ec2 as ec2
+from aws_cdk import aws_s3 as s3
+from typing import Optional
+from aws_cdk import (
+    aws_opensearchservice as os,
+    aws_cognito as cognito,
+)
+
+
+class RAGResources(Stack):
+    def __init__(
+        self,
+        scope: Construct,
+        id: str,
+        vpc_id: str,
+        cognito_pool_id: str,
+        os_domain_endpoint: str,
+        os_domain_port: str,
+        os_security_group_id: str,
+        os_index_name: str,
+        input_asset_bucket_name: Optional[str],
+        **kwargs,
+    ) -> None:
+        super().__init__(
+            scope,
+            id,
+            description=" This stack creates resources for the LLM - QA RAG ",
+            **kwargs,
+        )
+
+        # get an existing OpenSearch provisioned cluster
+        os_domain = os.Domain.from_domain_endpoint(
+            self,
+            "osdomain",
+            domain_endpoint="https://" + os_domain_endpoint,
+        )
+        self.os_domain = os_domain
+        # get vpc from vpc id
+        vpc = ec2.Vpc.from_lookup(
+            self,
+            "VPC",
+            vpc_id=vpc_id,
+        )
+
+        # get an existing userpool
+        cognito_pool_id = cognito_pool_id
+        user_pool_loaded = cognito.UserPool.from_user_pool_id(
+            self,
+            "myuserpool",
+            user_pool_id=cognito_pool_id,
+        )
+
+        if input_asset_bucket_name:
+            input_asset_bucket = s3.Bucket.from_bucket_name(
+                self, "input-assets-bucket", input_asset_bucket_name
+            )
+        else:
+            input_asset_bucket = None
+        # 1. Create Ingestion pipeline
+        rag_ingest_resource = RagAppsyncStepfnOpensearch(
+            self,
+            "RagAppsyncStepfnOpensearch",
+            existing_vpc=vpc,
+            existing_opensearch_domain=os_domain,
+            open_search_index_name=os_index_name,
+            cognito_user_pool=user_pool_loaded,
+            existing_input_assets_bucket_obj=input_asset_bucket,
+        )
+
+        self.security_group_id = rag_ingest_resource.security_group.security_group_id
+
+        self.rag_ingest_resource = rag_ingest_resource
+        # 2. create question and answer pipeline
+        rag_qa_source = QaAppsyncOpensearch(
+            self,
+            "QaAppsyncOpensearch",
+            existing_vpc=vpc,
+            existing_opensearch_domain=os_domain,
+            open_search_index_name=os_index_name,
+            cognito_user_pool=user_pool_loaded,
+            existing_input_assets_bucket_obj=rag_ingest_resource.s3_processed_assets_bucket,
+            existing_security_group=rag_ingest_resource.security_group,
+        )
+
+        security_group = rag_qa_source.security_group
+
+        os_security_group = ec2.SecurityGroup.from_security_group_id(
+            self, "OSSecurityGroup", os_security_group_id
+        )
+        os_security_group.add_ingress_rule(
+            peer=security_group,
+            connection=ec2.Port.tcp(int(os_domain_port)),
+            description="Allow inbound HTTPS to open search from embeddings lambda and question answering lambda",
+        )
+
+        self.rag_resource = rag_qa_source
