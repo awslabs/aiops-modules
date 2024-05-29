6e5cd4e8ebf63ef0a2db9af3f154b063b61ed509
Leon Luttenberger <luttenberger.leon@gmail.com>
2024-05-29 11:16:23 -0500

    chore: Refactor sagemaker-templates to use pydantic for inputs

---
+++ modules/sagemaker/sagemaker-templates-service-catalog/settings.py
@@ -0,0 +1,90 @@
+"""Defines the stack settings."""
+
+from abc import ABC
+from typing import Dict, List, Optional
+
+from pydantic import Field, computed_field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class CdkBaseSettings(BaseSettings, ABC):
+    """Defines common configuration for settings."""
+
+    model_config = SettingsConfigDict(
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        protected_namespaces=(),
+        extra="ignore",
+        populate_by_name=True,
+    )
+
+
+class ModuleSettings(CdkBaseSettings):
+    """Seedfarmer Parameters.
+
+    These parameters are required for the module stack.
+    """
+
+    model_config = SettingsConfigDict(env_prefix="SEEDFARMER_PARAMETER_")
+
+    portfolio_name: str = Field(default="MLOps SageMaker Project Templates")
+    portfolio_owner: str = Field(default="administrator")
+    portfolio_access_role_arn: str
+
+    dev_vpc_id: Optional[str] = Field(default=None)
+    dev_subnet_ids: Optional[List[str]] = Field(default=None)
+    dev_security_group_ids: Optional[List[str]] = Field(default=None)
+
+    pre_prod_account_id: Optional[str] = Field(default=None)
+    pre_prod_region: Optional[str] = Field(default=None)
+    pre_prod_vpc_id: Optional[str] = Field(default=None)
+    pre_prod_subnet_ids: Optional[List[str]] = Field(default=None)
+    pre_prod_security_group_ids: Optional[List[str]] = Field(default=None)
+
+    prod_account_id: Optional[str] = Field(default=None)
+    prod_region: Optional[str] = Field(default=None)
+    prod_vpc_id: Optional[str] = Field(default=None)
+    prod_subnet_ids: Optional[List[str]] = Field(default=None)
+    prod_security_group_ids: Optional[List[str]] = Field(default=None)
+
+    tags: Optional[Dict[str, str]] = Field(default=None)
+
+
+class SeedFarmerSettings(CdkBaseSettings):
+    """Seedfarmer Settings.
+
+    These parameters comes from seedfarmer by default.
+    """
+
+    model_config = SettingsConfigDict(env_prefix="SEEDFARMER_")
+
+    project_name: str = Field(default="")
+    deployment_name: str = Field(default="")
+    module_name: str = Field(default="")
+
+    @computed_field  # type: ignore
+    @property
+    def app_prefix(self) -> str:
+        """Application prefix."""
+        prefix = "-".join([self.project_name, self.deployment_name, self.module_name])
+        return prefix
+
+
+class CDKSettings(CdkBaseSettings):
+    """CDK Default Settings.
+
+    These parameters comes from AWS CDK by default.
+    """
+
+    model_config = SettingsConfigDict(env_prefix="CDK_DEFAULT_")
+
+    account: str
+    region: str
+
+
+class ApplicationSettings(CdkBaseSettings):
+    """Application settings."""
+
+    seedfarmer_settings: SeedFarmerSettings = Field(default_factory=SeedFarmerSettings)
+    module_settings: ModuleSettings = Field(default_factory=ModuleSettings)
+    cdk_settings: CDKSettings = Field(default_factory=CDKSettings)
