82f2da5a1ca6d00b91fe1e4bae7023518e195822
Leon Luttenberger <luttenberger.leon@gmail.com>
2024-05-29 14:23:32 -0500

    refactor settings to use model_dump

--- modules/sagemaker/sagemaker-templates-service-catalog/app.py
+++ modules/sagemaker/sagemaker-templates-service-catalog/app.py
@@ -12,27 +12,12 @@ app_settings = ApplicationSettings()
 
 stack = ServiceCatalogStack(
     app,
-    app_settings.seedfarmer_settings.app_prefix,
-    portfolio_name=app_settings.module_settings.portfolio_name,
-    portfolio_owner=app_settings.module_settings.portfolio_owner,
-    portfolio_access_role_arn=app_settings.module_settings.portfolio_access_role_arn,
-    dev_vpc_id=app_settings.module_settings.dev_vpc_id,
-    dev_subnet_ids=app_settings.module_settings.dev_subnet_ids,
-    dev_security_group_ids=app_settings.module_settings.dev_security_group_ids,
-    pre_prod_account_id=app_settings.module_settings.pre_prod_account_id,
-    pre_prod_region=app_settings.module_settings.pre_prod_region,
-    pre_prod_vpc_id=app_settings.module_settings.pre_prod_vpc_id,
-    pre_prod_subnet_ids=app_settings.module_settings.pre_prod_subnet_ids,
-    pre_prod_security_group_ids=app_settings.module_settings.pre_prod_security_group_ids,
-    prod_account_id=app_settings.module_settings.prod_account_id,
-    prod_region=app_settings.module_settings.prod_region,
-    prod_vpc_id=app_settings.module_settings.prod_vpc_id,
-    prod_subnet_ids=app_settings.module_settings.prod_subnet_ids,
-    prod_security_group_ids=app_settings.module_settings.prod_security_group_ids,
+    id=app_settings.seedfarmer_settings.app_prefix,
     env=aws_cdk.Environment(
         account=app_settings.cdk_settings.account,
         region=app_settings.cdk_settings.region,
     ),
+    **app_settings.module_settings.model_dump(),
 )
 
 aws_cdk.CfnOutput(
@@ -47,10 +32,6 @@ aws_cdk.CfnOutput(
 )
 
 aws_cdk.Aspects.of(stack).add(cdk_nag.AwsSolutionsChecks(log_ignores=True))
-
-if app_settings.module_settings.tags:
-    for tag_key, tag_value in app_settings.module_settings.tags.items():
-        aws_cdk.Tags.of(app).add(tag_key, tag_value)
 
 aws_cdk.Tags.of(app).add("SeedFarmerDeploymentName", app_settings.seedfarmer_settings.deployment_name)
 aws_cdk.Tags.of(app).add("SeedFarmerModuleName", app_settings.seedfarmer_settings.module_name)
