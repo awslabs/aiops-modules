59994dc89639b35ed27ee33452f5517fce4862b4
Patrick Cloke <pcloke@amazon.com>
2024-07-05 07:38:03 -0400

    Switch to comprehension.

--- modules/sagemaker/sagemaker-model-monitoring/sagemaker_model_monitoring/utils.py
+++ modules/sagemaker/sagemaker-model-monitoring/sagemaker_model_monitoring/utils.py
@@ -5,13 +5,5 @@ from typing import Any
 def generate_unique_id(*args: Any) -> str:
     """
     Generate a shortened hex digest from a list of arguments.
-
-    Any non-string arguments are cast to a string.
     """
-    hash = md5()
-    for arg in args:
-        if not isinstance(arg, str):
-            arg = str(arg)
-        hash.update(arg.encode())
-        hash.update(b"\x00")
-    return hash.hexdigest()[:10]
+    return md5(b"\x00".join(str(arg).encode() for arg in args)).hexdigest()[:10]
