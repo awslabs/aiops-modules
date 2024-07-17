eeff9b22f91376ff8fe1b326726f185d783ccb7b
Leon Luttenberger <luttenberger.leon@gmail.com>
2024-07-17 11:41:28 +0200

    enable lookups

--- modules/sagemaker/sagemaker-studio/integ/integ_studio.py
+++ modules/sagemaker/sagemaker-studio/integ/integ_studio.py
@@ -1,4 +1,5 @@
 import json
+import os
 import sys
 from typing import Dict, List
 
@@ -13,6 +14,7 @@ import stack  # noqa: E402
 
 app = cdk.App()
 
+
 def get_module_dependencies(resource_keys: List[str]) -> Dict[str, str]:
     ssm = boto3.client("ssm", region_name="us-east-1")
     dependencies = {}
@@ -22,6 +24,7 @@ def get_module_dependencies(resource_keys: List[str]) -> Dict[str, str]:
     except Exception as e:
         print(f"issue getting dependencies: {e}")
     return dependencies
+
 
 dependencies = get_module_dependencies(
     # Add any required resource identifiers here
@@ -51,6 +54,7 @@ integration.IntegTest(
     ],
     diff_assets=True,
     stack_update_workflow=True,
+    enable_lookups=True,
     cdk_command_options=cas.CdkCommands(
         deploy=cas.DeployCommand(args=cas.DeployOptions(require_approval=cas.RequireApproval.NEVER, json=True)),
         destroy=cas.DestroyCommand(args=cas.DestroyOptions(force=True)),
