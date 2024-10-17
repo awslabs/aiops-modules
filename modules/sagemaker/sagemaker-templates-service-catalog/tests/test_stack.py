3b4c27c133baa1b38666d35255565f1db194a4cf
Ashish Bhatt <acbhatt@amazon.com>
2024-10-17 11:43:30 +0530

    Updated README with prerequesites information and updated unit tests

--- modules/sagemaker/sagemaker-templates-service-catalog/tests/test_stack.py
+++ modules/sagemaker/sagemaker-templates-service-catalog/tests/test_stack.py
@@ -54,10 +54,8 @@ def stack(stack_defaults) -> cdk.Stack:
     sagemaker_domain_id = "domain_id"
     sagemaker_domain_arn = f"arn:aws:sagemaker:::domain/{sagemaker_domain_id}"
     repository_type = RepositoryType.CODECOMMIT
-    access_token_secret_name = ("github_token",)
-    aws_codeconnection_arn = (
-        "arn:aws:codeconnections:xxxxxx:xxxxxxxxxxxx:connection/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
-    )
+    access_token_secret_name = "github_token"
+    aws_codeconnection_arn = "arn:aws:codeconnections:xxxxxx:xxxxxxxxxxxx:connection/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
     repository_owner = "github-test-owner"
 
     return stack.ServiceCatalogStack(
