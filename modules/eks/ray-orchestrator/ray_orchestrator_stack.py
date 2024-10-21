9fdf03bd41cbf306f30f3d3a9da21162d6b6d8a3
Anton Kukushkin <kukushkin.anton@gmail.com>
2024-10-21 14:45:34 +0100

    add ray orchestrator module

---
+++ modules/eks/ray-orchestrator/ray_orchestrator_stack.py
@@ -0,0 +1,127 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+import logging
+from typing import Any, cast
+
+from aws_cdk import Duration, Stack, Tags
+from aws_cdk import aws_eks as eks
+from aws_cdk import aws_iam as iam
+from aws_cdk import aws_logs as logs
+from aws_cdk import aws_stepfunctions as sfn
+from aws_cdk.lambda_layer_kubectl_v29 import KubectlV29Layer
+from constructs import Construct, IConstruct
+
+_logger: logging.Logger = logging.getLogger(__name__)
+
+
+class RayOrchestrator(Stack):
+    def __init__(
+        self,
+        scope: Construct,
+        id: str,
+        *,
+        project_name: str,
+        deployment_name: str,
+        module_name: str,
+        eks_cluster_name: str,
+        eks_admin_role_arn: str,
+        eks_cluster_endpoint: str,
+        eks_openid_connect_provider_arn: str,
+        eks_cert_auth_data: str,
+        namespace_name: str,
+        service_account_name: str,
+        service_account_role_arn: str,
+        **kwargs: Any,
+    ) -> None:
+        self.project_name = project_name
+        self.deployment_name = deployment_name
+        self.module_name = module_name
+
+        super().__init__(
+            scope,
+            id,
+            **kwargs,
+        )
+
+        dep_mod = f"{project_name}-{deployment_name}-{module_name}"
+        # used to tag AWS resources. Tag Value length can't exceed 256 characters
+        full_dep_mod = dep_mod[:256] if len(dep_mod) > 256 else dep_mod
+        Tags.of(scope=cast(IConstruct, self)).add(key="Deployment", value=full_dep_mod)
+
+        provider = eks.OpenIdConnectProvider.from_open_id_connect_provider_arn(
+            self, "OIDCProvider", eks_openid_connect_provider_arn
+        )
+
+        eks.Cluster.from_cluster_attributes(
+            self,
+            "EKSCluster",
+            cluster_name=eks_cluster_name,
+            open_id_connect_provider=provider,
+            kubectl_role_arn=eks_admin_role_arn,
+            kubectl_layer=KubectlV29Layer(self, "Kubectlv29Layer"),
+        )
+
+        service_account_role = iam.Role.from_role_arn(self, "ServiceAccountRole", service_account_role_arn)
+
+        body = {
+            "apiVerson": "batch/v1",
+            "kind": "Job",
+            "metadata": {
+                "namespace": namespace_name,
+                "name.$": "States.Format('job-{}', $$.Execution.Name)",
+            },
+            "spec": {
+                "backoffLimit": 1,
+                "template": {
+                    "spec": {
+                        "restartPolicy": "Never",
+                        "serviceAccountName": service_account_name,
+                        "containers": [
+                            {
+                                "name": "ray-hello-world-1",
+                                "image": "python:3.9.19",
+                                "command": [
+                                    "sh",
+                                    "-c",
+                                    (
+                                        'pip install ray"[client]"==2.30.0 && python -c '
+                                        '"import ray; '
+                                        "ray.init(address='ray://kuberay-head-svc:10001'); "
+                                        'print(ray.cluster_resources())"'
+                                    ),
+                                ],
+                            }
+                        ],
+                    },
+                },
+            },
+        }
+
+        ek_run_job_state = sfn.CustomState(
+            self,
+            "EksRunJobState",
+            state_json={
+                "Type": "Task",
+                "Resource": "arn:aws:states:::eks:runJob.sync",
+                "Parameters": {
+                    "ClusterName": eks_cluster_name,
+                    "Namespace": namespace_name,
+                    "CertificateAuthority": eks_cert_auth_data,
+                    "Endpoint": eks_cluster_endpoint,
+                    "LogOptions": {"RetrieveLogs": True},
+                    "Job": body,
+                },
+            },
+        )
+
+        self.log_group = logs.LogGroup(self, "JobLogGroup")
+
+        self.sm = sfn.StateMachine(  # noqa: F841
+            self,
+            "EKSRunJob",
+            definition_body=sfn.DefinitionBody.from_chainable(sfn.Chain.start(ek_run_job_state)),
+            timeout=Duration.minutes(15),
+            logs=sfn.LogOptions(destination=self.log_group, level=sfn.LogLevel.ALL),
+            role=service_account_role,
+        )
---
+++ modules/eks/ray-orchestrator/rbac_stack.py
@@ -0,0 +1,192 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+import logging
+from typing import Any, Optional, cast
+
+from aws_cdk import Stack, Tags
+from aws_cdk import aws_eks as eks
+from aws_cdk import aws_iam as iam
+from aws_cdk.lambda_layer_kubectl_v29 import KubectlV29Layer
+from constructs import Construct, IConstruct
+
+_logger: logging.Logger = logging.getLogger(__name__)
+
+
+class RbacStack(Stack):
+    def __init__(
+        self,
+        scope: Construct,
+        id: str,
+        *,
+        project_name: str,
+        deployment_name: str,
+        module_name: str,
+        eks_cluster_name: str,
+        eks_admin_role_arn: str,
+        eks_oidc_arn: str,
+        eks_handler_role_arn: str,
+        namespace_name: str,
+        data_bucket_name: Optional[str],
+        **kwargs: Any,
+    ) -> None:
+        self.project_name = project_name
+        self.deployment_name = deployment_name
+        self.module_name = module_name
+        self.namespace_name = namespace_name
+
+        super().__init__(
+            scope,
+            id,
+            description="This stack deploys EKS RBAC Configuration",
+            **kwargs,
+        )
+
+        dep_mod = f"{project_name}-{deployment_name}-{module_name}"
+        # used to tag AWS resources. Tag Value length can't exceed 256 characters
+        full_dep_mod = dep_mod[:256] if len(dep_mod) > 256 else dep_mod
+        Tags.of(scope=cast(IConstruct, self)).add(key="Deployment", value=full_dep_mod)
+
+        # Import EKS Cluster
+        provider = eks.OpenIdConnectProvider.from_open_id_connect_provider_arn(self, "Provider", eks_oidc_arn)
+        handler_role = iam.Role.from_role_arn(self, "HandlerRole", eks_handler_role_arn)
+        eks_cluster = eks.Cluster.from_cluster_attributes(
+            self,
+            "EKSCluster",
+            cluster_name=eks_cluster_name,
+            kubectl_role_arn=eks_admin_role_arn,
+            open_id_connect_provider=provider,
+            kubectl_lambda_role=handler_role,
+            kubectl_layer=KubectlV29Layer(self, "Kubectlv29Layer"),
+        )
+
+        service_account = eks_cluster.add_service_account(
+            "OrchestratorServiceAccount", name=module_name, namespace=namespace_name
+        )
+        self.service_account = service_account
+
+        service_account_role: iam.Role = cast(iam.Role, service_account.role)
+        if data_bucket_name:
+            service_account_role.add_to_policy(
+                iam.PolicyStatement(
+                    effect=iam.Effect.ALLOW,
+                    actions=[
+                        "s3:PutObject",
+                        "s3:GetObject",
+                        "s3:AbortMultipartUpload",
+                        "s3:ListBucket",
+                        "s3:GetObjectVersion",
+                        "s3:ListMultipartUploadParts",
+                    ],
+                    resources=[
+                        f"arn:{self.partition}:s3:::{data_bucket_name}/*",
+                        f"arn:{self.partition}:s3:::{data_bucket_name}",
+                    ],
+                )
+            )
+
+        service_account_role.assume_role_policy.add_statements(
+            iam.PolicyStatement(
+                effect=iam.Effect.ALLOW,
+                actions=["sts:AssumeRole"],
+                principals=[iam.ServicePrincipal("states.amazonaws.com")],
+            )
+        )
+
+        eks_cluster.add_manifest(
+            "rbac-orchestrator-role",
+            {
+                "apiVersion": "rbac.authorization.k8s.io/v1",
+                "kind": "Role",
+                "metadata": {
+                    "name": module_name,
+                    "namespace": namespace_name,
+                },
+                "rules": [{"apiGroups": ["*"], "resources": ["*"], "verbs": ["*"]}],
+            },
+        )
+
+        rbac_role_binding = eks_cluster.add_manifest(
+            "rbac-orchestrator-role-binding",
+            {
+                "apiVersion": "rbac.authorization.k8s.io/v1",
+                "kind": "RoleBinding",
+                "metadata": {"name": module_name, "namespace": namespace_name},
+                "roleRef": {
+                    "apiGroup": "rbac.authorization.k8s.io",
+                    "kind": "Role",
+                    "name": module_name,
+                },
+                "subjects": [
+                    {"kind": "User", "name": module_name},
+                    {
+                        "kind": "ServiceAccount",
+                        "name": module_name,
+                        "namespace": namespace_name,
+                    },
+                ],
+            },
+        )
+        rbac_role_binding.node.add_dependency(service_account)
+
+        eks_cluster.add_manifest(
+            "rbac-orchestrator-role-default",
+            {
+                "apiVersion": "rbac.authorization.k8s.io/v1",
+                "kind": "Role",
+                "metadata": {"name": f"default-{module_name}", "namespace": "default"},
+                "rules": [
+                    {
+                        "apiGroups": ["*"],
+                        "resources": ["*"],
+                        "verbs": ["get", "list", "watch"],
+                    }
+                ],
+            },
+        )
+
+        rbac_role_binding = eks_cluster.add_manifest(
+            "rbac-orchestrator-role-binding-default",
+            {
+                "apiVersion": "rbac.authorization.k8s.io/v1",
+                "kind": "RoleBinding",
+                "metadata": {"name": f"default-{module_name}", "namespace": "default"},
+                "roleRef": {
+                    "apiGroup": "rbac.authorization.k8s.io",
+                    "kind": "Role",
+                    "name": f"default-{module_name}",
+                },
+                "subjects": [
+                    {"kind": "User", "name": module_name},
+                    {
+                        "kind": "ServiceAccount",
+                        "name": module_name,
+                        "namespace": namespace_name,
+                    },
+                ],
+            },
+        )
+        rbac_role_binding.node.add_dependency(service_account)
+
+        rbac_cluster_role_binding = eks_cluster.add_manifest(
+            "rbac-orchestrator-cluster-role-binding",
+            {
+                "apiVersion": "rbac.authorization.k8s.io/v1",
+                "kind": "ClusterRoleBinding",
+                "metadata": {"name": f"system-access-{module_name}"},
+                "roleRef": {
+                    "apiGroup": "rbac.authorization.k8s.io",
+                    "kind": "ClusterRole",
+                    "name": f"system-access-{module_name}",
+                },
+                "subjects": [
+                    {"kind": "User", "name": module_name},
+                    {
+                        "kind": "ServiceAccount",
+                        "name": module_name,
+                        "namespace": namespace_name,
+                    },
+                ],
+            },
+        )
+        rbac_cluster_role_binding.node.add_dependency(service_account)
