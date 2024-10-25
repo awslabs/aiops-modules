b547736dc8f91769f0bd4d1cbc113db088798e09
Anton Kukushkin <kukushkin.anton@gmail.com>
2024-10-25 12:39:16 +0100

    remove unneeded scripts

--- manifests/fine-tuning-6b/scripts/yolov11-hyperparams-optimize.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-# Load a YOLO11n model
-model = YOLO("yolo11n.pt")
-
-# Start tuning hyperparameters for YOLO11n training on the COCO8 dataset
-result_grid = model.tune(data="coco8.yaml", use_ray=True)
-
-print(result_grid)
--- manifests/fine-tuning-6b/scripts/yolov11-train.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO("yolo11s")  # load a pretrained model (recommended for training)
-
-# Train the model
-results = model.train(data="coco8.yaml", epochs=100, imgsz=640)
